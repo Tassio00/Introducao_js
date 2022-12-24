224b6634ad48b1b6b986bbf9cd01733804061120
Tassio00 <tassio.h.a@gmail.com>
2022-12-24 14:23:32 -0300

    continuação operadores

--- js/operadores.js
+++ js/operadores.js
@@ -30,7 +30,7 @@ console.log(1 == '1'); //true
 
 
 //      Operador Ternário:
-// Um cliente, se tiver + de 100pts = premium, - de 100pts = comum
+// ex: Um cliente, se tiver + de 100pts = premium, - de 100pts = comum
 let pontos = 100;
 let tipo = pontos >= 100 ? 'premium': 'comum'; 
 console.log(tipo);//se a operação for true (primeiro elemento). Se for false (segundo elemento)
@@ -38,7 +38,42 @@ console.log(tipo);//se a operação for true (primeiro elemento). Se for false (
 
 //      operadores lógicos:
 
-//Operador (e) = &&  [retorna TRUE se os dois operadores forem true]
+//Operador (e) = &&  [retorna TRUE se os dois operandos forem true]
+
+console.log(true && true); //true
+console.log(true && false); //false
+// ex: Só pode tirar a carteira se for maior de idade
+let maioridade = false;
+let possuiCarteira = true;
+let podeDirigir = maioridade && possuiCarteira;
+console.log('Pode dirigir: ', podeDirigir); //false
+
+//Operador (ou) = ||  [retorna TRUE se um dos operanos forem true]
+
+let podeDirigir2 = maioridade || possuiCarteira;
+console.log('Pode dirigir2: ', podeDirigir2); //true
+
+//Operador (not) = !  [retorna tudo que não for o operando]
+
+let motoristaRecusado = !podeDirigir2;
+console.log('Recusado: ', motoristaRecusado);
+
+
+
+// COMPARAÇÔES COM VALORES NÂO BOLEANOS
+
+//Falsy:
+//undefined
+//null
+// 0
+// false
+// ''
+// NaN - not a number (volores inválidos)
+
+//Truthy: [Tudos os tipos que não são os tipos acima ^]
+
+
+
 
 
 
